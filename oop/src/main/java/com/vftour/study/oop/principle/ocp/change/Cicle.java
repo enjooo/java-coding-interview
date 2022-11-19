1033d453365f34a0cd47deac01259e75e483ed02
尹东东 <wordall1101@126.com>
2022-11-19 11:58:22 +0800

    完成开闭原则编写

--- oop/src/main/java/com/vftour/study/oop/principle/ocp/change/Cicle.java
+++ oop/src/main/java/com/vftour/study/oop/principle/ocp/change/Cicle.java
@@ -25,8 +25,8 @@ public class Cicle implements Shape {
 
     @Override
     public int compareTo(Shape o) {
-        //1表示第二个对象比前一个大，0表示其相等，-1表示比前一个小
-        return o.getSort() - sort;
+        //从大到小排序
+        return o.getSort() - this.getSort();
     }
 
     @Override
